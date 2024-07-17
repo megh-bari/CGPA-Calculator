c00cd8da69a625da014cfc8ee88571d3ce99f86b
megh-bari <meghbari01@gmail.com>
2024-07-17 20:19:40 +0530

    overall completed

--- script.js
+++ script.js
@@ -7,18 +7,16 @@ form.addEventListener("submit", function (e) {
   const sem2 = parseFloat(document.querySelector("#sem2").value);
   const result = document.querySelector(".result");
 
-  if (sem1 === "" || sem1 < 0 || isNaN(sem1)) {
-    result.innerHTML = "Please Enter Valid Sem-1 Pointer";
-  } else if (sem2 === "" || sem2 < 0 || isNaN(sem2)) {
-    result.innerHTML = "Please Enter Valid Sem-2 Pointer";
+  if (sem1 === "" || sem1 < 0 || isNaN(sem1) || sem1 > 10) {
+    result.innerHTML = "Please Enter Valid Sem-1 SGPA (0-10)";
+  } else if (sem2 === "" || sem2 < 0 || isNaN(sem2) || sem2 > 10) {
+    result.innerHTML = "Please Enter Valid Sem-2 SGPA (0-10)";
   } else {
-    // Calculate CGPA
+    //* Calculate CGPA
     const totalSGPA = sem1 + sem2;
-    const numberOfSemesters = 2; // Assuming only two semesters for simplicity
+    const numberOfSemesters = 2; 
     const cgpa = totalSGPA / numberOfSemesters;
 
-    // Display the CGPA
     result.innerHTML = `Your CGPA is: ${cgpa.toFixed(2)}`;
   }
 });
-
